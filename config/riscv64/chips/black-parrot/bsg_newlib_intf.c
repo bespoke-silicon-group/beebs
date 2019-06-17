76ec62f058de16a88513598f87cd6859e0ee690d
Farzam Gilani <gilanifarzam@gmail.com>
2019-06-17 10:59:45 -0700

    start code cleanup

--- config/riscv64/chips/black-parrot/bsg_newlib_intf.c
+++ config/riscv64/chips/black-parrot/bsg_newlib_intf.c
@@ -3,25 +3,12 @@
 
 void bsg_newlib_init(void) {
 
-    // Init file system
-    if(bsg_newlib_fs_init() < 0) {
-      exit(-1);
-    }
 }
 
 void bsg_newlib_exit(int exit_status) {
-  //EXIT_SUCCESS is 0 
-  if(exit_status == EXIT_SUCCESS) {
-    exit(0);
-  } else {
-    exit(-1);
-  }
+
 }
 
 void bsg_newlib_sendchar(char ch) {
-  char* ch_ptr;
-  ch_ptr = 0x8FFFFFFF;
-  *ch_ptr = ch;
-  //putchar(ch);
 
 }
